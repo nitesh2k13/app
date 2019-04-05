c03d57d493d98fb26deb618d13e4045b763b9ad7
Nitesh Kumar <Nitesh Kumar@NiteshKumar-PC>
2019-04-05 15:15:21 +0530

    my choice

---
+++ src/app/Employee.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Employee {
+	private int id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
+
+}
